fa42c9f130a5eda35b6f5215241cb38e9545c1d2
blanhhy <balancestar676@qq.com>
2026-05-25 09:30:28 +0000

    Fix `assets` path error

--- assets/templates/conf.lua
+++ assets/templates/conf.lua
@@ -1,6 +1,4 @@
--- Conf Constants
-GAME_NAME = "Template Game"
-PKG_NAME = "com.example.mygame"
+require "init"
 
 function love.conf(t)
     t.window.title = GAME_NAME
--- src/assets/templates/conf.lua
+++
@@ -1,7 +0,0 @@
-require "init"
-
-function love.conf(t)
-    t.window.title = GAME_NAME
-    t.identity = PKG_NAME
-    t.externalstorage = true
-end
